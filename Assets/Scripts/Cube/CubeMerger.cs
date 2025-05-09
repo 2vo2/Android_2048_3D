8f6b61bdf67e87a779551595dcf224b979df187b
Yehor <egorzvoznikov@gmail.com>
2025-05-09 12:06:52 +0300

    add toss value for cube merger

--- Assets/Scripts/Cube/CubeMerger.cs
+++ Assets/Scripts/Cube/CubeMerger.cs
@@ -8,6 +8,7 @@ namespace Cube
     {
         [SerializeField] private CubeUnit _cubeUnit;
         [SerializeField] private float _minImpulseValueForMerge;
+        [SerializeField] private float _tossMergeCubeValue;
         
         public event Action<int, Vector3> Merge;
         public event Action<Vector3> Hit;
@@ -26,16 +27,22 @@ namespace Cube
                 
                     var mergeValue = _cubeUnit.CubeNumber / 2;
                     Score.Instance.AddScore(mergeValue);
-                
+
                     Merge?.Invoke(_cubeUnit.CubeNumber * 2, other.contacts[0].point);
 
-                    _cubeUnit.Rigidbody.AddForce(Vector3.up * 10, ForceMode.Impulse);
+                    TossMergeCube();
                 }
                 else
                 {
                     Hit?.Invoke(other.contacts[0].point);
                 }
             }
+        }
+
+        private void TossMergeCube()
+        {
+            var tossVector = new Vector3(0f, 1f, 1f);
+            _cubeUnit.Rigidbody.AddForce(tossVector * _tossMergeCubeValue, ForceMode.Impulse);
         }
     }
 }
